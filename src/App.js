52918515b76a1c52ea447f5c77154756c39b8d70
ravikiran <ravikiransjce.jobs@gmail.com>
2021-06-01 22:01:54 +0530

    Made the page respoonsive to mobile resoltion

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import PageInfo from './components/PageInfo';
-import PageImage from './components/PageImage';
+import PageInfo from "./components/PageInfo";
+import PageImage from "./components/PageImage";
 
 function App() {
   return (
     <div className="container-fluid border">
-        <div className="row pageHeading">
-          <div className="col-sm-12 pageHeadingLogo">
-            <img src="/images/logo.svg" alt="logo"/>
-          </div>
+      <div className="row pageHeading">
+        <div className="col-sm-12">
+          <img src="/images/logo.svg" alt="logo" className="pageHeadingLogo" />
         </div>
-        <div className="row">
-            <PageImage></PageImage>
-            <PageInfo></PageInfo>
-        </div>
+      </div>
+      <div className="row">
+        <PageImage></PageImage>
+        <PageInfo></PageInfo>
+      </div>
     </div>
   );
 }
--- src/components/PageImage.js
+++ src/components/PageImage.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const PageImage = () => {
-    return (
-        <div className="col-md-4 col-sm-12 order-sm-2 border pageImage">
-            
-        </div>
-    )
-}
+  return <div className="col-md-4 col-sm-12 order-sm-2 pageImage"></div>;
+};
 
-export default PageImage
+export default PageImage;
